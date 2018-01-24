8d5a88fb07991bd1fdc2743833d03d824fce0514
pavel.alshevskyi <herdemisemyrise.bt@gmail.com>
2018-01-24 17:56:18 +0200

    Initial commit

---
+++ mobile/src/main/java/com/example/nexus/golift/FirstFragment.java
@@ -0,0 +1,25 @@
+package com.example.nexus.golift;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nexus on 1/24/18.
+ */
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onViewCreated(LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.example_fragment, container, false);
+
+
+    }
+}
